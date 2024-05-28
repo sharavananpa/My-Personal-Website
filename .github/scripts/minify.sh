21938b1bc4bc9700057cd248db5468234b41b1fb
Sharavanan <sharavananpa@gmail.com>
2024-05-28 20:49:25 +0530

    Modify deployment script

--- .github/scripts/minify.sh
+++ .github/scripts/minify.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
 npm install gulp gulp-htmlmin gulp-clean-css gulp-uglify gulp-rename gulp-rev gulp-rev-replace del --save-dev
-
 gulp
 
 mv CNAME dist/CNAME
+rm *
+mv -v ./dist/* .
+rm dist
+rm .gitignore
+rm -r .github
